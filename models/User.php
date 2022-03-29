34bf60d5b8e329d3d1c10296f548768b44e3b40f
Isaias Rodrigo <lima.isaias@aluno.ifsp.edu.br>
2022-03-29 17:53:40 -0300

    update, sectall e delete criados

--- models/User.php
+++ models/User.php
@@ -37,5 +37,32 @@ class User{
         }
         echo "Delete no banco".$this->id;
     }
+    function update(){
+        $db = new Database();
+        try {
+            $stmt = $db->conn->prepare("UPDATE users SET name = :name, email = :email, pass = :pass
+            WHERE id = :id");
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindParam(':pass', $this->pass);
+            $stmt->execute();
+            echo 'User atualizado com sucesso!';
+        }catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+    function selectAll(){
+        $db = new Database();
+        try {
+            $stmt = $db->conn->prepare("SELECT * FROM users; ");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            print_r($result);
+            echo 'User atualizado com sucesso!';
+        }catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 }
 ?>
