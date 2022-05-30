b88c37b8a56141760bdd334be031f822ede09bb2
ISAIAS <cg3004121@LAB117M18.IFSP>
2022-05-30 17:58:19 -0300

    Atualizei

--- helpers/Output.php
+++ helpers/Output.php
@@ -1,8 +1,9 @@
 <?php
 class Output{
+
     function out($result = [], $code = 200){
-        header('Content-type: application/json; charset="utf-8');
-        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json; charset=utf-8');
+        header("Access-Control-Allow-Origin: *");
         http_response_code($code);
         echo json_encode($result);
         die;
@@ -10,10 +11,8 @@ class Output{
 
     function allowedMethod($method){
         if($method != $_SERVER['REQUEST_METHOD']){
-            http_response_code(405);
             $result['message'] = "Method not allowed for this route.";
-            echo json_encode($result);
-            die;
+            $this->out($result, 405);
         }
     }
 }
